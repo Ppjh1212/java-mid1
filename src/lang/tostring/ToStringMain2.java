9d871e83aafae86b79d03da3dbc0a2b1ef2f0bc4
Ppjh1212 <145564710+Ppjh1212@users.noreply.github.com>
2024-05-20 21:18:14 +0900

    2

---
+++ src/lang/tostring/ToStringMain2.java
@@ -0,0 +1,29 @@
+package lang.tostring;
+
+public class ToStringMain2 {
+
+    public static void main(String[] args) {
+        Car car = new Car("Model Y");
+        Dog dog1 = new Dog("멍멍1", 2);
+        Dog dog2 = new Dog("멍멍2", 5);
+
+        //단순 toString 호출
+        System.out.println(car.toString());
+        System.out.println(dog1.toString());
+        System.out.println(dog2.toString());
+
+        //println 내부에서 toString 호출
+        System.out.println(car);
+        System.out.println(dog1);
+        System.out.println(dog2);
+
+        //Object 다형성 활용
+        ObjectPrinter.print(car);
+        ObjectPrinter.print(dog1);
+        ObjectPrinter.print(dog2);
+
+        //dog1 참조값
+        String dog1RefValue = Integer.toHexString(System.identityHashCode(dog1));
+        System.out.println(dog1RefValue);
+    }
+}
